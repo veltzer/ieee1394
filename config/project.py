7e40f33a49c7555fc9ab8be745a15f7a9e383ad5
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 05:20:30 +0300

    fixing the build

---
+++ config/project.py
@@ -0,0 +1,4 @@
+project_name = "ieee1394"
+project_long_description = "ieee1394 stack for old linux kernels" 
+project_year_started = 2011
+project_description =  "ieee1394 stack for old linux kernels"
