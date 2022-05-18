e7d718d03820eb2677be9d68e65dedfc8a53e1a9
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:51:00 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "ieee1394"
-project_long_description = "ieee1394 stack for old linux kernels" 
-project_year_started = 2011
-project_description =  "ieee1394 stack for old linux kernels"
+description_short = "ieee1394 stack for old linux kernels" 
